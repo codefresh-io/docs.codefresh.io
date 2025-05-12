2b33583e9b99a5852852999c178862640b94d6b7
Alina Shklyar <alina.shklyar@codefresh.io>
2025-05-12 10:37:52 +0300

    removed comment

--- assets/js/src/argohub-redirect.js
+++ assets/js/src/argohub-redirect.js
@@ -22,7 +22,7 @@ async function getArgoHubRedirectURL(currentPath) {
   return newURL;
 }
 
-async function handleRedirect() {
+async function handleRedirect () {
   handleEnterpriseDocTypeLock()
 
   if (shouldSkipRedirect()) return;
